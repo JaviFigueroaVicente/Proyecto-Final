e06419b49df77d2c8d53bc555f5c2e29b344d6ad
Javier <javierfigueroa969@gmail.com>
2024-05-24 18:29:21 +0200

    buscador prueba

--- car-flex/MongoDB/header.php
+++ car-flex/MongoDB/header.php
@@ -7,7 +7,9 @@
 </head>
 <body>
 <?php
+
 include("Conexion_MongoDB.php");
+
     if(!isset($_SESSION['usuari_nom'])){
     echo'<section id="MenuLogin">
         <div>
@@ -23,12 +25,20 @@ include("Conexion_MongoDB.php");
         </nav>
     </section>';
     }else{ 
+    //filtro de búsqueda
+    
     $perfilUsuario = $coleccion->findOne(["Usuario" => $_SESSION['usuari_nom']]);
     $FotoPerfil = $perfilUsuario["fotoPerfil"];      
     echo '<section id="MenuLogin">
     <div>
         <a href="index.php"><img src="img/Logo_Car_Flex.webp" alt="Logo">
         <h1>Car Flex</h1></a>
+    </div>
+    <div>
+        <form action="" id="busqueda" method="get">
+            <label for="busqueda"><input type="text" placeholder="Que quieres buscar?" id="form_busqueda" name="form_busqueda">
+            <input type="submit" value="Buscar">
+        </form>
     </div>
     <nav id="MenuPrincipal">
         <ul>
@@ -40,6 +50,16 @@ include("Conexion_MongoDB.php");
     </nav>
 </section>';
     }
+    $busqueda = isset($_GET['form_busqueda']) ? $_GET['form_busqueda'] : "";
+
+    // Si hay un término de búsqueda, filtrar por usuarioPubli
+    if (!empty($busqueda)) {
+        $publicacionesUsuarios = $publicaciones->find(['usuarioPubli' => $busqueda]);
+    } else {
+        // Si no hay término de búsqueda, recuperar todas las publicaciones ordenadas por hora
+        $publicacionesUsuarios = $publicaciones->find([], ['sort' => ['hora' => -1]]);
+    }
+$filtro = isset($_GET['filtro']) ? $_GET['filtro'] : 'recientes';
 ?>
 </body>
 
--- car-flex/MongoDB/index_include.php
+++ car-flex/MongoDB/index_include.php
@@ -1,12 +1,6 @@
 <?php
 include("Conexion_MongoDB.php");
 
-/*$buscador="";
-if(isset($_POST["buscador"])){
-    $buscador=$_POST["buscador"];
-}*/
-$filtro = isset($_GET['filtro']) ? $_GET['filtro'] : 'recientes';
-
 if ($filtro=='tendencias'){
     $publicacionesUsuarios = $publicaciones->find([], ['sort' => ['likes' => -1, 'hora' => -1]]);
 }else if($filtro=='gustados'){
